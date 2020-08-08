b90a6fff869e6b1e0ec316ae255332c847023dc7
Paul Evon <paulevon13@gmail.com>
2020-08-08 19:19:04 +0300

    iniatl excel logic done

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -3,13 +3,26 @@ import { DomListener } from "./DOMListener";
 export class ExcelComponent extends DomListener {
 	constructor($root, options = {}) {
 		super($root, options.listeners)
+		this.name = options.name || ''
+		this.emitter = options.emitter
+		this.prepare()
+		this.unsubscribeList = []
 	}
-	// returns component template 
-
+	prepare() { }
+	$dispatch(event, ...args) {
+		this.emitter.dispatch(event, ...args)
+	}
+	$on(event, fn) {
+		const unsub = this.emitter.subscribe(event, fn)
+		this.unsubscribeList.push(unsub)
+	}
 	init() {
 		this.initDomListeners()
 	}
 	destroy() {
 		this.removeDomListeners()
+		this.unsubscribeList.forEach(unsub => {
+			unsub()
+		})
 	}
 } 
